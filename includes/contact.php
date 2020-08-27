03e09cc05058a6e9f990bfd250671b8f897aea6a
Sushantchalise0 <sushant.chalise0@gmail.com>
2020-08-27 17:49:41 +0545

    Frontend with pages made clean, first commit

---
+++ includes/contact.php
@@ -0,0 +1,38 @@
+
+    <section id="contact">
+        <div id="google-map" style="height:650px" data-latitude="27.682964" data-longitude="85.319947"></div>
+        <div class="container-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-8 col-sm-6 col-sm-offset-6">
+                        <div class="contact-form">
+                            <h3>Contact Info</h3>
+
+                            <address>
+                              <strong>Center For Information & Techonology</strong><br>
+                              Pulchowk Campus, IOE<br>
+                              Patan Dhoka, Lalitpur<br>
+                              <abbr title="Phone">Phone:</abbr> (01) 5547120
+                            </address>
+
+                            <form id="main-contact-form" name="contact-form" method="post" action="#">
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" placeholder="Name" required>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control" placeholder="Email" required>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="subject" class="form-control" placeholder="Subject" required>
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="message" class="form-control" rows="8" placeholder="Message" required></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Send Message</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ index.php
@@ -0,0 +1,98 @@
+<php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Center For Information and Techonology</title>
+    <!-- core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.transitions.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<!--/head-->
+
+<body id="home" class="homepage">
+
+        <?php include "includes/navigation.php" ?>
+
+    <!--/header-->
+
+        <?php include "includes/slider.php" ?>
+        
+    <!--/#main-slider-->
+
+    <!--/#cta-->
+
+    <?php include "includes/features.php" ?>
+
+    <?php include "includes/container_box.php" ?>
+
+    <?php include "includes/services.php" ?>
+    <!--/#services-->
+
+    <?php include "includes/works.php" ?>
+    <!--/#portfolio-->
+
+    <?php include "includes/about.php" ?>
+    <!--/#about-->
+
+    <?php include "includes/process.php" ?>
+    <!--/#work-process-->
+
+    <?php include "includes/teams.php" ?>
+    <!--/#meet-team-->
+
+    <?php include "includes/facts.php" ?>
+    <!--/#animated-number-->
+
+    <?php include "include/pricing.php" ?>
+    <!--/#pricing-->
+
+    <?php include "includes/testimonials.php" ?>
+    <!--/#testimonial-->
+
+    <?php include "includes/blogs.php" ?>
+
+    <?php include "includes/touch.php" ?>
+    <!--/#get-in-touch-->
+
+
+    <?php include "includes/contact.php" ?>
+    <!--/#bottom-->
+
+<?php include "includes/footer.php"  ?>
+    <!--/#footer-->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/mousescroll.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/jquery.inview.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
